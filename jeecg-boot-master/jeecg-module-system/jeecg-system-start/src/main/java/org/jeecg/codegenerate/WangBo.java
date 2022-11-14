4da27d7c5485b11ad9c7066b10bde4832c43fbda
wangbo <1851128158@qq.com>
2022-11-14 17:22:05 +0800

    王波 增加

---
+++ jeecg-boot-master/jeecg-module-system/jeecg-system-start/src/main/java/org/jeecg/codegenerate/WangBo.java
@@ -0,0 +1,10 @@
+package org.jeecg.codegenerate;/*
+ *ClassName:WangBo
+ *Package:org.jeecg.codegenerate
+ *Description:
+ *@Date:2022/11/14 17:21
+ *@Author:1851128158@qq.com
+ */
+
+public class WangBo {
+}
